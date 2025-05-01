a826d912e1c6b40465dd97d4d4a8489eece6b087
Prabhu <Prabhucsc7@gmail.com>
2025-05-01 13:35:28 +0530

    Task2

--- src/main/java/jenkinsDay1/BasicRunFromSelenium.java
+++ src/main/java/jenkinsDay1/BasicRunFromSelenium.java
@@ -20,8 +20,12 @@ public class BasicRunFromSelenium {
 		driver.findElement(By.id("password")).sendKeys("Leaf$123");
 		driver.findElement(By.id("Login")).click();
 		
-		//CTRL+A
+	
 		act.keyDown(Keys.CONTROL).sendKeys("A").keyUp(Keys.CONTROL).perform();
+		
+		//CTRL+V
+		act.keyDown(Keys.CONTROL).sendKeys("V").keyUp(Keys.CONTROL).perform();
+
 		
 		driver.quit();
 	
