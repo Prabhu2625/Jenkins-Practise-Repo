1c43ccb8420df6cc55366000e685a997662c629d
Prabhu <Prabhucsc7@gmail.com>
2025-05-01 10:59:00 +0530

    First commit

---
+++ JenkinsSample/src/main/java/jenkinsDay1/BasicRunFromSelenium.java
@@ -0,0 +1,27 @@
+package jenkinsDay1;
+
+import java.time.Duration;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicRunFromSelenium {
+
+	@Test
+	public void login() {
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://login.salesforce.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("username")).sendKeys("gokul.sekar@testleaf.com");
+		driver.findElement(By.id("password")).sendKeys("Leaf$123");
+		driver.findElement(By.id("Login")).click();
+		driver.quit();
+	
+
+	}
+		
+	}
+
