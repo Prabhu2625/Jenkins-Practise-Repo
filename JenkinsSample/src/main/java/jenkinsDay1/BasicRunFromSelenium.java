20f43edfc75103b939faded718325cc8b5e4e634
Prabhu <Prabhucsc7@gmail.com>
2025-05-01 12:04:38 +0530

    Second Commit

--- JenkinsSample/src/main/java/jenkinsDay1/BasicRunFromSelenium.java
+++ JenkinsSample/src/main/java/jenkinsDay1/BasicRunFromSelenium.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicRunFromSelenium {
@@ -18,6 +19,10 @@ public class BasicRunFromSelenium {
 		driver.findElement(By.id("username")).sendKeys("gokul.sekar@testleaf.com");
 		driver.findElement(By.id("password")).sendKeys("Leaf$123");
 		driver.findElement(By.id("Login")).click();
+		
+		//CTRL+A
+		act.keyDown(Keys.CONTROL).sendKeys("A").keyUp(Keys.CONTROL).perform();
+		
 		driver.quit();
 	
 
